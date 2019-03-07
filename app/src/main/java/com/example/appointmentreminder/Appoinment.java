1f1e739d365c6b8904bc50328d53ae3739548191
Rao Moueed Ahmed <moueedrao@gmail.com>
2019-03-07 16:40:53 +0500

    Rename Appoinment to Appointment and change in class body

--- app/src/main/java/com/example/appointmentreminder/Appoinment.java
+++ app/src/main/java/com/example/appointmentreminder/Appoinment.java
@@ -2,88 +2,26 @@ package com.example.appointmentreminder;
 
 public class Appoinment {
 
-    private  String name;
-    private  String type;
-    private int dayDate;
-    private int yearDate;
-    private  String monthDate;
-    private int hourTime;
-    private int minuteTime;
-    private String AMorPMTime;
+    public String name;
+    public String type;
+    public String monthDate;
+    public int dayDate;
+    public int yearDate;
+    public int hourTime;
+    public int minuteTime;
+    public String AMorPMTime;
 
-    public Appoinment(String name, String type, int dayDate, int yearDate, String monthDate,
-                      int hourTime, int minuteTime, String AMorPMTime) {
-        this.name = name;
-        this.type = type;
-        this.dayDate = dayDate;
-        this.yearDate = yearDate;
-        this.monthDate = monthDate;
-        this.hourTime = hourTime;
-        this.minuteTime = minuteTime;
-        this.AMorPMTime = AMorPMTime;
+    public Appoinment(String passedAppointmentName, String passedAppointType,
+                      String passedAppointmentDateMonth, int passedAppointmentDateDay, int passedAppointmentDateYear,
+                      int passedAppointmentTimeHour, int passedAppointmentTimeMinute, String passedAppointmentTimeAMorPM){
+        name = passedAppointmentName;
+        type = passedAppointType;
+        monthDate = passedAppointmentDateMonth;
+        dayDate = passedAppointmentDateDay;
+        yearDate = passedAppointmentDateYear;
+        hourTime = passedAppointmentTimeHour;
+        minuteTime = passedAppointmentTimeMinute;
+        AMorPMTime = passedAppointmentTimeAMorPM;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public int getDayDate() {
-        return dayDate;
-    }
-
-    public void setDayDate(int dayDate) {
-        this.dayDate = dayDate;
-    }
-
-    public int getYearDate() {
-        return yearDate;
-    }
-
-    public void setYearDate(int yearDate) {
-        this.yearDate = yearDate;
-    }
-
-    public String getMonthDate() {
-        return monthDate;
-    }
-
-    public void setMonthDate(String monthDate) {
-        this.monthDate = monthDate;
-    }
-
-    public int getHourTime() {
-        return hourTime;
-    }
-
-    public void setHourTime(int hourTime) {
-        this.hourTime = hourTime;
-    }
-
-    public int getMinuteTime() {
-        return minuteTime;
-    }
-
-    public void setMinuteTime(int minuteTime) {
-        this.minuteTime = minuteTime;
-    }
-
-    public String getAMorPMTime() {
-        return AMorPMTime;
-    }
-
-    public void setAMorPMTime(String AMorPMTime) {
-        this.AMorPMTime = AMorPMTime;
-    }
 }
