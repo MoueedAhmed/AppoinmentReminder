f3ac82b9301deeb84d6828ba2a9b827b60a096d2
Rao Moueed Ahmed <moueedrao@gmail.com>
2019-03-09 18:11:22 +0500

    Add: Dialog onCreateDialog(int id)
    Click on date TextView and time TextView opens Dialog for Date and Time and set them on TextViews on the screen.

--- app/src/main/java/com/example/appointmentreminder/AddAppointmentActivity.java
+++ app/src/main/java/com/example/appointmentreminder/AddAppointmentActivity.java
@@ -1,8 +1,14 @@
 package com.example.appointmentreminder;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -28,6 +34,22 @@ public class AddAppointmentActivity extends AppCompatActivity {
 
         txtDate = findViewById(R.id.textViewDate);
         txtTime = findViewById(R.id.textViewTime);
+
+        //When Date Text View is touched, opens a Date Picker Dialog
+        txtDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DATE_DIALOG_ID);
+            }
+        });
+
+        //When Time Text View is touched, opens a Time Picker Dialog
+        txtTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(TIME_DIALOG_ID);
+            }
+        });
         //set current date and time
         setCurrentDateAndTimeOnView();
     }
@@ -61,5 +83,50 @@ public class AddAppointmentActivity extends AppCompatActivity {
         else
             return "0" + String.valueOf(c);
     }
+
+    private DatePickerDialog.OnDateSetListener datePickerListener
+            = new DatePickerDialog.OnDateSetListener() {
+
+        // when dialog box is closed, below method will be called.
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+            year = selectedYear;
+            month = selectedMonth;
+            day = selectedDay;
+
+            // set selected date into textview
+            txtDate.setText(new StringBuilder().append(month + 1)
+                    .append("-").append(day).append("-").append(year)
+                    .append(" "));
+        }
+    };
+
+    private TimePickerDialog.OnTimeSetListener timePickerListener
+            = new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int selectedHour, int selectedMinute) {
+            hour = selectedHour;
+            minute = selectedMinute;
+
+            // set current time into textview
+            txtTime.setText(new StringBuilder().append(pad(hour))
+                    .append(":").append(pad(minute)));
+        }
+    };
+
+    //Displays a new dialog for date picker or time picker
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_DIALOG_ID:
+                return new DatePickerDialog(this,
+                        datePickerListener, year, month,day);
+            case TIME_DIALOG_ID:
+                // set time picker as current time
+                return new TimePickerDialog(this,
+                        timePickerListener, hour, minute,false);
+        }
+        return null;
+    }
+
 
 }
